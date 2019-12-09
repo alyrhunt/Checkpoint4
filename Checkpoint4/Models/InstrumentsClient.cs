092573cb4353c154703a179095bb1943a659f9da
Alyssa Rae Anderson <aly97@byu.edu>
2019-12-09 15:17:15 -0700

    Model within Model

---
+++ Checkpoint4/Models/InstrumentsClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Checkpoint4.Models
+{
+    public class InstrumentsClient
+    {
+        public Client client { get; set; }
+        public Instrument instruments { get; set; }
+
+    }
+}
